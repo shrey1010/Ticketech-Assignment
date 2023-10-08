287091313f2a4b76f576b0d3b39d0e3181b8741b
Shrey <shreyshukla1010@gmail.com>
2023-10-08 18:19:02 +0530

    resolve bugs

--- todo_app/ToDo/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import TODO
-
-
-class TODOForm(ModelForm):
-    class Meta:
-        model = TODO
-        fields = ['title', 'status', 'priority']
